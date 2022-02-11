5041e26923b99cee8df617c2dc49203efeb02ff5
keerthana <20b01a1242@svecw.edu.in>
2022-02-11 22:02:57 +0530

    table

---
+++ src/table.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Faker from "faker";
+
+
+
+function Table(props) {
+   
+
+    console.log(props);
+    return (
+        
+      <table className="ui very basic collapsing celled table">
+    <thead>
+      <tr><th>Employee</th>
+      <th>Correct Guesses</th>
+    </tr></thead>
+    <tbody>
+      <tr>
+        <td>
+          <h4 className="ui image header">
+            <img src={Faker.image.image()} className="ui mini rounded image" />
+            <div className="content">
+              {props.name}
+              <div className="sub header">Human Resources
+            </div>
+          </div>
+        </h4></td>
+        <td>
+          22
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h4 className="ui image header">
+            <img src={Faker.image.image()} className="ui mini rounded image" />
+            <div className="content">
+              Matthew
+              <div className="sub header">Fabric Design
+            </div>
+          </div>
+        </h4></td>
+        <td>
+          15
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h4 className="ui image header">
+            <img src={Faker.image.image()} className="ui mini rounded image" />
+            <div className="content">
+              Lindsay
+              <div className="sub header">Entertainment
+            </div>
+          </div>
+        </h4></td>
+        <td>
+          12
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h4 className="ui image header">
+            <img src={Faker.image.image()} className="ui mini rounded image" />
+            <div className="content">
+              Mark
+              <div className="sub header">Executive
+            </div>
+          </div>
+        </h4></td>
+        <td>
+          11
+        </td>
+      </tr>
+    </tbody>
+    
+  </table>
+  
+
+  );
+  }
+  export default Table;
